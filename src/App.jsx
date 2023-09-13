011dda3dbd042a6962a42835132560623fad872f
M4RCKUX <marcossalinas@uoc.edu>
2023-09-13 19:47:02 +0200

    First Commit of Memo App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className={'flex flex-col items-center py-14'}>
-        <h1 className={'bg-amber-100 font-bold text-gray-900 text-4xl p-4'}>Vite React Tailwind Jest Babel</h1>
+        <h1 className={'bg-amber-100 font-bold text-gray-900 text-4xl p-4'}>Memo App</h1>
       </div>
     </>
   )
